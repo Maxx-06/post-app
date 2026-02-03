8d912d39b3826cec0409a391482a6a0efb4343bd
Ujjwal Bhardwaj <ujjwalbhardwajrok.06@gmail.com>
2026-02-03 23:28:27 +0530

    Updated server.js logic

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
 const connectDB = require('./src/db/db')
 const app = require('./src/app')
-
+const PORT = 5000;
 
 
 connectDB()
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("Server is running at http://localhost:3000")
 })
